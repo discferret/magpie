dace4cdfff47eda31574024bc89e9f6a60f5b51a
Philip Pemberton <philpem@philpem.me.uk>
2011-02-11 03:37:16 +0000

    add near-complete drivespec parser

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <map>
 #include <iostream>
+#include <algorithm>
 #include <getopt.h>
 
 // DiscFerret
@@ -37,19 +38,31 @@ class CDriveInfo {
 	private:
 		string			_drive_type;		///< Drive type string (immutable)
 		string			_friendly_name;		///< Friendly name (displayed to user)
-		unsigned long	_steprate_us;		///< Step rate in milliseconds
+		unsigned long	_steprate_us;		///< Step rate in microseconds
 		unsigned long	_spinup_ms;			///< Spin-up wait time in milliseconds
 		unsigned long	_tracks;			///< Number of tracks
 		unsigned long	_track_step;		///< Steps per track (1=single-stepped, 2=double-stepped)
 		unsigned long	_heads;				///< Number of heads
 	public:
-		const string drive_type()			{ return _drive_type;		};
-		const string friendly_name()		{ return _friendly_name;	};
-		const unsigned long steprate_us()	{ return _steprate_us;		};
-		const unsigned long spinup_ms()		{ return _spinup_ms;		};
-		const unsigned long tracks()		{ return _tracks;			};
-		const unsigned long track_step()	{ return _track_step;		};
-		const unsigned long heads()			{ return _heads;			};
+		const string drive_type()				{ return _drive_type;		};
+		void drive_type(const string x)			{ _drive_type = x;			};
+		const string friendly_name()			{ return _friendly_name;	};
+		void friendly_name(const string x)		{ _friendly_name = x;		};
+		const unsigned long steprate_us()		{ return _steprate_us;		};
+		void steprate_us(const unsigned long x)	{ _steprate_us = x;			};
+		const unsigned long spinup_ms()			{ return _spinup_ms;		};
+		void spinup_ms(const unsigned long x)	{ _spinup_ms = x;			};
+		const unsigned long tracks()			{ return _tracks;			};
+		void tracks(const unsigned long x)		{ _tracks = x;				};
+		const unsigned long track_step()		{ return _track_step;		};
+		void track_step(const unsigned long x)	{ _track_step = x;			};
+		const unsigned long heads()				{ return _heads;			};
+		void heads(const unsigned long x)		{ _heads = x;				};
+
+		/// No-args ctor for CDriveInfo
+		CDriveInfo()
+		{
+		}
 
 		/**
 		 * ctor for CDriveInfo.
@@ -81,12 +94,27 @@ class CDriveInfo {
 };
 
 /**
+ * Exception class for DriveSpec parse errors
+ */
+class EDriveSpecParse : public exception {
+	private:
+		string x;
+		unsigned long _spec;
+	public:
+		EDriveSpecParse(const string error, const unsigned long specnum) : exception(), x(error), _spec(specnum) { };
+		virtual ~EDriveSpecParse() throw() {};
+
+		virtual const char* what() throw() { return x.c_str(); }
+		virtual const unsigned long spec() { return _spec; }
+};
+
+/**
  * Drive information storage map.
  *
  * Stores a list of all available drive types, mapped by drive type string.
  * The type string is also stored in CDriveInfo for
  */
-map<string, CDriveInfo> drives;
+map<string, CDriveInfo> mDrives;
 
 /// Verbosity flag; true if verbose mode enabled.
 int bVerbose = false;
@@ -98,12 +126,8 @@ extern "C" {
 	LUALIB_API int luaopen_bit(lua_State *L);
 }
 
-int LoadDriveScript(const string filename)
+lua_State *SetupLua(void)
 {
-	int err;
-
-	if (bVerbose) cout << "loading lua script: " << filename;
-
 	// Set up Lua
 	// TODO: error checking!
 	lua_State *L = luaL_newstate();
@@ -131,7 +155,17 @@ int LoadDriveScript(const string filename)
 		lua_setglobal(L, LCONSTS[i].name.c_str());
 	}
 
+	return L;
+}
+
+int LoadDriveScript(const string filename)
+{
+	int err;
+
+	if (bVerbose) cout << "loading lua script: " << filename;
+
 	// Load the script into Lua
+	lua_State *L = SetupLua();
 	err = luaL_dofile(L, filename.c_str());
 	if (err) {
 		cerr << "Error loading Lua script: " << lua_tostring(L, -1) << endl;
@@ -140,9 +174,104 @@ int LoadDriveScript(const string filename)
 		return -1;
 	}
 
-	// TODO: set up the drive specs
+	// Scan through all the Drive Specs in this file -- TODO: error check
+	lua_getfield(L, LUA_GLOBALSINDEX, "drivespecs");
+	lua_pushnil(L);		// first key
+	while (lua_next(L, -2) != 0) {
+		// uses 'key' at index -2, and 'value' at index -1
 
-	// TODO: REMOVEME: test calling
+		// Check that 'value' is a table
+		try {
+			if (lua_istable(L, -1)) {
+				// Temporary storage for drivespec fields (CDriveInfo's mandatory parameters are immutable)
+				string drivetype = "$$unspecified$$";
+				string friendlyname = "$$unspecified$$";
+				unsigned int heads = 1,
+							 tracks = 40,
+							 trackstep = 1,
+							 spinup = 1000,
+							 steprate = 6000;
+
+				// Now we parse the DriveSpec -- we do this using the same table iteration method we use above
+				lua_pushnil(L);		// Initial key
+				while (lua_next(L, -2) != 0) {
+					// Sort out where this needs to go in our 'temporary drivespec'
+					//
+					// Get the parameter name and convert it to lower case
+					string key = lua_tostring(L, -2);
+					transform(key.begin(), key.end(), key.begin(), ::tolower);
+
+					// Convert the key->value pairs into local variables, with error checking
+					if (key.compare("drivetype") == 0) {
+						// [string] Drive type
+						drivetype = lua_tostring(L, -1);
+						if (drivetype.length() == 0)
+							throw EDriveSpecParse("drivetype not valid.", lua_tointeger(L, -4));
+					} else if (key.compare("friendlyname") == 0) {
+						// [string] Friendly name
+						friendlyname = lua_tostring(L, -1);
+						if (friendlyname.length() == 0)
+							throw EDriveSpecParse("friendlyname not valid.", lua_tointeger(L, -4));
+					} else if (key.compare("heads") == 0) {
+						// [integer] Number of heads
+						heads = lua_tointeger(L, -1);
+						if (heads < 1)
+							throw EDriveSpecParse("Value of 'heads' parameter must be an integer greater than zero.", lua_tointeger(L, -4));
+					} else if (key.compare("spinup") == 0) {
+						// [integer] Spinup time, milliseconds
+						spinup = lua_tointeger(L, -1);
+					} else if (key.compare("steprate") == 0) {
+						// [float] Step rate, milliseconds
+						double x = lua_tonumber(L, -1);
+						steprate = x * 1000;	// convert from milliseconds to microseconds
+						if ((steprate < 250) || (steprate > (255*250)))
+							throw EDriveSpecParse("Value of 'steprate' parameter must be between 0.25 and 63.75.", lua_tointeger(L, -4));
+					} else if (key.compare("tracks") == 0) {
+						// [integer] Number of tracks
+						tracks = lua_tointeger(L, -1);
+						if (tracks < 1)
+							throw EDriveSpecParse("Value of 'tracks' parameter must be an integer greater than zero.", lua_tointeger(L, -4));
+					} else if (key.compare("trackstep") == 0) {
+						// [integer] Number of physical tracks to step for each logical track
+						trackstep = lua_tointeger(L, -1);
+						if (trackstep < 1)
+							throw EDriveSpecParse("Value of 'trackstep' parameter must be an integer greater than zero.", lua_tointeger(L, -4));
+					} else {
+						throw EDriveSpecParse("Unrecognised key \"" + key + "\"", lua_tointeger(L, -4));
+					}
+
+					// pop value off of stack, leave key for next iteration
+					lua_pop(L, 1);
+				}
+
+				// Now we have all our keys, try to make a CDriveInfo
+				if (drivetype.compare("$$unspecified$$") == 0)
+					throw EDriveSpecParse("Drivetype string not specified.", lua_tointeger(L, -4));
+				if (friendlyname.compare("$$unspecified$$") == 0)
+					throw EDriveSpecParse("Friendlyname string not specified.", lua_tointeger(L, -4));
+				CDriveInfo d(drivetype, friendlyname, steprate, spinup, tracks, trackstep, heads);
+
+				// TODO: put the CDriveInfo into the global map
+			} else {
+				// This isn't a table... what does the user think they're playing at?
+				throw EDriveSpecParse("drivespecs table contains a non-table entity.", lua_tointeger(L, -4));
+				lua_close(L);
+				return -1;
+			}
+		} catch (EDriveSpecParse &e) {
+			// Parse error / format violation. Let the user know what (we think) they did wrong...
+			cerr << "DriveSpec format violation (#" << e.spec() << "): " << e.what() << endl;
+			return -1;
+		}
+
+		// remove 'value' from stack, keep 'key' for next iteration
+		lua_pop(L, 1);
+	}
+
+	// pop the table off of the stack
+	lua_pop(L, 1);
+
+	// TODO: REMOVEME: test calling ==> should be in a separate function we can use to get drive info
 	lua_getfield(L, LUA_GLOBALSINDEX, "isDriveReady");
 	lua_pushstring(L, "cdc-94205-51");	// TODO: push drivetype here
 	lua_pushnumber(L, DISCFERRET_STATUS_DENSITY + DISCFERRET_STATUS_DISC_CHANGE);	// TODO: push status here
